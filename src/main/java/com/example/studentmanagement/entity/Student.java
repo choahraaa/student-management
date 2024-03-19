27d79e4646a8b0ea74508dac25f80bf312ef10ac
ahracho <ahra123@i-commtech.com>
2024-03-19 11:47:23 +0900

    id entity add

--- src/main/java/com/example/studentmanagement/entity/Student.java
+++ src/main/java/com/example/studentmanagement/entity/Student.java
@@ -1,14 +1,17 @@
 package com.example.studentmanagement.entity;
 
 import com.example.studentmanagement.enums.school.SchoolType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
 @Setter
+@Builder
 public class Student {
 
+    private String id;
     private String name;
-    private String age;
+    private int age;
     private SchoolType schoolType;
     private String phone;
 }
